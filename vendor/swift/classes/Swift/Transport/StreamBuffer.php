cd2b99ec5e791906ee3ae869823707a013d0c9ac
Denis Malykh <mrdekk@yandex.ru>
2014-07-07 15:26:47 +0600

    fix: kohana-email

--- vendor/swift/classes/Swift/Transport/StreamBuffer.php
+++ vendor/swift/classes/Swift/Transport/StreamBuffer.php
@@ -52,6 +52,8 @@ class Swift_Transport_StreamBuffer
   /** Translations performed on data being streamed into the buffer */
   private $_translations = array();
   
+  private $_sequence = 0;
+  
   /**
    * Create a new StreamBuffer using $replacementFactory for transformations.
    * @param Swift_ReplacementFilterFactory $replacementFactory
